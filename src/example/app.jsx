296240fba24928f20b5594a0962502a54aecf06d
kirill3333 <kirill3333@inbox.ru>
2017-10-01 17:38:46 +0300

    usage example

---
+++ src/example/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Avatar from '../lib/avatar.jsx';
+
+ReactDOM.render(
+  <Avatar
+    width={384}
+    height={458}
+    src={'http://topmira.com/images/5/models-2015/Кара%20Делевинь%20%20Cara%20Delevingne%20фото.jpg'}
+  />,
+  document.getElementById('root')
+)
