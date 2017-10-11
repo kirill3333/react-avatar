2695c92542e27f03cf9e5683001612f306a34310
kirill3333 <kirill3333@inbox.ru>
2017-10-11 19:17:50 +0300

    split builds to prod and dev

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = {
+  entry: './src/avatar.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'react-avatar.js',
+    library: 'reactAvatar',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ]
+  },
+  externals: {
+    'react': 'react'
+  },
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+}
