78a5ef787972dd235fe3aeac60f80334c5f58b45
David <dg@pododesk.de>
2020-10-25 14:05:13 +0100

    add property to disable the close button

--- src/avatar.d.ts
+++ src/avatar.d.ts
@@ -24,12 +24,12 @@ export interface Props {
    * The height of the editor (image will fit to this height)
    */
   height: number;
-  
+
   /**
    * The desired width of the image, can not be used together with imageHeight
    */
   imageWidth?: number;
-  
+
   /**
    * The desired height of the image, can not be used together with imageWidth
    */
@@ -114,7 +114,7 @@ export interface Props {
    * Invoked when user drag&drop event stop and return croped image in base64 sting
    */
   onCrop?: (data: string) => void;
-  
+
   /**
    * Invoked when user upload file with internal file loader
    */
@@ -129,6 +129,12 @@ export interface Props {
    * Invoked when user clock on close editor button
    */
   onClose?: () => void;
+
+  /**
+   * Disable the close button
+   * Default: false
+   */
+  disableCloseButton?: boolean;
 }
 
 declare class Avatar extends React.Component<Props> {
